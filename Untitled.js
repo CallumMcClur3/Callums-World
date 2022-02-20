c8ed9ba01d49bceba5ee73e615198ec45b5adf0e
CallumMcClur3 <100099992+CallumMcClur3@users.noreply.github.com>
2022-02-20 23:56:04 +0000

    Add files via upload

---
+++ Untitled.js
@@ -0,0 +1,17 @@
+console.log("This code will run on my machine")
+
+Devon = ["Exeter",
+"Plymouth",
+"Exmouth"
+];
+
+console.log(Devon[0])
+console.log(Devon[Devon.length - 1])
+Devon.push("Newton Abbot")
+console.log(Devon[Devon.length - 1])
+Devon.pop();
+console.log(Devon[Devon.length - 1])
+console.log(Devon)
+console.log(Devon.shift());
+console.log(Devon
+    )
